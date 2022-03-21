73d04b83e2b11dbe93ee3527b1288ee5f585bb24
keer4n <keer4n@protonmail.com>
2022-03-21 00:37:34 -0600

    add basic text to image function

--- src/demos/TextToImage.svelte
+++ src/demos/TextToImage.svelte
@@ -1,65 +1,87 @@
 <script lang="ts">
-import { onMount } from 'svelte';
+    import CollapsibleSeparator from "../components/CollapsibleSeparator.svelte";
 
-   import CollapsibleSeparator from '../components/CollapsibleSeparator.svelte';
-
-   let canvas, h = 200, w = 200, t="Hello", s;
-   $: {
-       if(canvas){
-           canvas.height = h;
-           canvas.width = w;
-           fillCanvas();
-           writeText(t,s);
+    let canvas,
+        h = 200,
+        w = 200,
+        t = "Hello",
+        s = "60";
+    $: {
+        if (canvas) {
+            canvas.height = h;
+            canvas.width = w;
+            fillCanvas();
+            writeText(t, s);
         }
-   }
+    }
 
-   const fillCanvas = () => {
-       const ctx = canvas.getContext('2d');
-       ctx.fillStyle = 'blue';
-       ctx.fillRect(0,0, canvas.width, canvas.height);
-   };
+    const fillCanvas = () => {
+        const ctx = canvas.getContext("2d");
+        ctx.fillStyle = "blue";
+        ctx.fillRect(0, 0, canvas.width, canvas.height);
+    };
 
-   const writeText = (text, size) => {
-       const ctx = canvas.getContext('2d');
-       ctx.fillStyle = 'black';
-       ctx.font = `${size}px serif`;
-       ctx.textAlign = 'center';
-       ctx.textBaseline = 'middle';
-       let measure = ctx.measureText(text);
-       ctx.fillText(text, canvas.width/2, canvas.height/2, canvas.width )
-   }
+    const writeText = (text, size) => {
+        const ctx = canvas.getContext("2d");
+        ctx.fillStyle = "black";
+        ctx.font = `${size}px serif`;
+        ctx.textAlign = "center";
+        ctx.textBaseline = "middle";
+        let measure = ctx.measureText(text);
+        ctx.fillText(text, canvas.width / 2, canvas.height / 2, canvas.width);
+    };
 
-   onMount(() => {
-       fillCanvas();
-   })
-    
+    const downloadImage = () => {
+        canvas.toBlob((b) => {
+            const url = URL.createObjectURL(b);
+            const anchor = document.createElement('a');
+            anchor.href = url;
+            anchor.download = 'preview.png';
+            anchor.click();
+        })
+    }
 </script>
 
 <div class="container">
     <div class="content">
-        <canvas class="canvas" bind:this={canvas}></canvas>
+        <canvas class="canvas" bind:this={canvas} />
     </div>
 
-    <CollapsibleSeparator collapseTo="right"/>
+    <CollapsibleSeparator collapseTo="right" />
 
     <div class="settings">
         <fieldset>
             <label for="height">
-                <input name="height" type="number" bind:value={h}>
+                Height
+                <input name="height" type="number" bind:value={h} />
             </label>
             <label for="width">
-                <input name="width" type="number" bind:value={w}>
+                Width
+                <input name="width" type="number" bind:value={w} />
             </label>
             <label for="textContent">
-                <input name="textContent" type="text" bind:value={t}>
+                Text
+                <input name="textContent" type="text" bind:value={t} />
             </label>
 
             <label for="fontSize">
-                <input name="fontSize" type="number" bind:value={s}>
+                Font Size
+                <input name="fontSize" type="number" bind:value={s} />
             </label>
-
         </fieldset>
-
+        <div class="download">
+            <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                fill="gray"
+                on:click={downloadImage}
+                ><path
+                    d="M15.003 3h2.997v5h-2.997v-5zm8.997 1v20h-24v-24h20l4 4zm-19 5h14v-7h-14v7zm16 4h-18v9h18v-9z"
+                /></svg
+            >
+        </div>
     </div>
 </div>
 
@@ -67,24 +89,49 @@ import { onMount } from 'svelte';
     .container {
         display: flex;
         flex-direction: row;
-        height: 200px;
+        height: 100vh;
     }
 
     .content {
         display: flex;
         width: 50%;
         flex-grow: 1;
-        background-color: aqua;
         align-items: center;
         justify-content: center;
     }
-    
-    .settings{
-        background-color: bisque; 
+
+    .settings {
+        background-color: #ddd;
         flex-grow: 1;
     }
-    
+
     .canvas {
-        margin: 0 auto; 
+        margin: 0 auto;
+    }
+
+    label {
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+    }
+
+    label input {
+        margin: auto 0;
+    }
+
+    fieldset {
+        display: flex;
+        flex-direction: column;
+        gap: 0.5em;
+    }
+
+    .download {
+        padding: 1em;
+    }
+
+    .download svg:hover {
+        fill: black;
+        opacity: .9;
+        cursor: pointer;
     }
 </style>
