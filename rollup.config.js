9c377e5ce5918add5e7220a9e03fa2c967983a3c
keer4n <keer4n@protonmail.com>
2022-03-21 00:40:43 -0600

    run build

--- rollup.config.js
+++ rollup.config.js
@@ -36,7 +36,7 @@ export default {
 		sourcemap: true,
 		format: 'iife',
 		name: 'app',
-		file: 'public/build/bundle.js'
+		file: production? 'docs/build/bundle.js':'public/build/bundle.js'
 	},
 	plugins: [
 		svelte({
